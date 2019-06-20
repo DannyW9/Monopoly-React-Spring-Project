5fd9314d4b82b6b4bdf557b604d75aed8942166a
CalumCannon <calbo90@googlemail.com>
2019-06-20 18:57:27 +0100

    adds renderer container with canvas

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -1,58 +1,62 @@
 import React, {Component} from 'react';
 import Square from '../components/Square'
+import Renderer from '../containers/Renderer'
 //A unit is one tile width
 //Board is 13 units wide... 9 Property slots then 2 slots at each end for corner slots
 class Board extends Component {
   render(){
     return(
-      <div className="Board">
-        <div className="BottomRow">
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
+      <div>
+        <Renderer />
+        <div className="Board">
+          <div className="BottomRow">
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square type="Corner"/>
+          </div>
+          <div className="LeftColumn">
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square type="Corner"/>
+          </div>
+          <div className="TopRow">
           <Square type="Corner"/>
-        </div>
-        <div className="LeftColumn">
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square type="Corner"/>
-        </div>
-        <div className="TopRow">
-        <Square type="Corner"/>
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-        </div>
-        <div className="RightColumn">
-          <Square type="Corner"/>
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+          </div>
+          <div className="RightColumn">
+            <Square type="Corner"/>
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+          </div>
         </div>
       </div>
     )
---
+++ src/containers/Renderer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Renderer extends Component {
+
+  render(){
+    return (
+          <canvas className="canvas" id="myCanvas" width="600" height="600"></canvas>
+    )
+  }
+
+}
+
+export default Renderer;
