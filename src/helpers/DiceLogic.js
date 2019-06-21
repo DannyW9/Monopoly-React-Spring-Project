9e305510c9105a1d23d8853219134653d8df6434
Danny Welsh <dannywelsh400@hotmail.com>
2019-06-21 23:06:25 +0100

    Adds ability to render dice face images on roll

--- src/helpers/DiceLogic.js
+++ src/helpers/DiceLogic.js
@@ -27,6 +27,24 @@ const diceLogic = {
   actionsIfNotDouble(newValue, props){
     props.updatePlayerPosition(newValue)
     props.updateRolled()
+  },
+
+  showDiceFace(firstDiceValue){
+    switch (firstDiceValue) {
+      case 1:
+        return 'images/dice-one.png';
+      case 2:
+        return 'images/dice-two.png';
+      case 3:
+        return 'images/dice-three.png';
+      case 4:
+        return 'images/dice-four.png';
+      case 5:
+        return 'images/dice-five.png';
+      case 6:
+        return 'images/dice-six.png';
+
+    }
   }
 
 }
