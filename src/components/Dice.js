7feae9b67354de0c8741ab9419f3b065471c3692
Danny Welsh <dannywelsh400@hotmail.com>
2019-06-21 14:01:16 +0100

    adds function for getting a random dice roll

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -3,11 +3,31 @@ import React, {Component} from 'react';
 
 class Dice extends Component{
 
+  constructor(props){
+    super(props)
+    this.state = {
+      doubleRollMessage: ""
+    }
+    // this.handleRollDiceClick = this.handleRollDiceClick.bind(this)
+  }
+
+  getRandomRoll(){
+    return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+  }
+
+
 
 
   render(){
     return(
-    )
+      <div className="dice-div">
+        <button onClick={this.handleRollDiceClick}> Roll Dice </button>
+        <p> {this.getRandomRoll()} </p>
+        <p> {this.getRandomRoll()} </p>
+      </div>
+    );
+
+
   }
 
 }
