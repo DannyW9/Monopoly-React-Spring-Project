6a5d6c54a50a3f353dd72c7d9e538fd5b1c7b0c7
Danny Welsh <dannywelsh400@hotmail.com>
2019-06-23 14:38:32 +0100

    Saves End turn button

---
+++ src/components/EndTurn.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class EndTurn extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.handleEndTurnClick = this.handleEndTurnClick.bind(this)
+  }
+
+  handleEndTurnClick(){
+    this.props.updateActivePlayer()
+  }
+
+  render(){
+    return(
+      <button onClick={this.handleEndTurnClick}> End Turn </button>
+    )
+  }
+
+}
+
+export default EndTurn
