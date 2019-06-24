44afc66887f328009637759816aaf09efffdbd5a
Danny Welsh <dannywelsh400@hotmail.com>
2019-06-24 10:40:09 +0100

    Creates Card Class. Creates methods for adding money, subtracting money and moving the player

---
+++ src/models/Card.js
@@ -0,0 +1,35 @@
+class Card {
+
+  constructor(text, method, adjustor){
+    this.text = text,
+    this.method = method,
+    this.adjustor = adjustor
+  }
+
+  // CARD METHODS
+
+  addMoney(player){
+    player.money += this.adjustor;
+  }
+
+  subtractMoney(player){
+    player.money -= this.adjustor;
+  }
+
+  movePlayer(player){
+    // Check for specific card
+    if (this.text === "Go back 3 spaces."){
+      player.position -= 3
+    } else {
+      // Move to specified position
+      player.position = this.adjustor
+      // Check if this will make the player pass go
+      if ((player.position > this.adjustor) && (this.adjustor != 10)){
+        player.money += 200
+      }
+    }
+  }
+
+  
+
+}
