aae2572711728d272059e5a99d0e938938e5d55b
CalumCannon <calbo90@googlemail.com>
2019-06-24 15:43:17 +0100

    adds playerinterface

---
+++ src/components/PlayerInterface.js
@@ -0,0 +1,17 @@
+import React, {Component, useState}  from 'react';
+
+class PlayerInterface extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div className="playerInterface">
+      <p>PLAYER INTERFACE</p>
+      </div>
+    )
+  }
+}
+
+export default PlayerInterface;
