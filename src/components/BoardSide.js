dbba7fde0fc0b29bf3654f2665ddf87f9fd26229
CalumCannon <calbo90@googlemail.com>
2019-06-25 12:16:14 +0100

    removes more console logs, gives boardsqaures keys

--- src/components/BoardSide.js
+++ src/components/BoardSide.js
@@ -5,7 +5,6 @@ import Square from './Square.js';
 const BoardSide = (props) => {
 
   if(!props){
-    console.log("No props");
     return (<p>Loading</p>)
   }
 
@@ -19,21 +18,20 @@ const BoardSide = (props) => {
   function isColour(colour){
     var style = new Option().style;
     style.color = colour;
-    console.log(colour, style.color === colour);
     return style.color == colour;
   }
 
   function handleSquare(square){
     //Check if setColour != color
     if(!isColour(square.setColor)){
-      return <Square card={square} type="non-set" />
+      return <Square key={square.squareNumber} card={square} type="non-set" />
     }
     //if not colour go to handle non setSqaure
-    return  <Square card={square}/>
+    return  <Square key={square.squareNumber} card={square}/>
   }
 
   function handleCorner(square){
-    return   <Square card={square} type={"Corner"} cornerImage={props.cornerImage} />
+    return   <Square key={square.squareNumber} card={square} type={"Corner"} cornerImage={props.cornerImage} />
   }
 
 	const squares = props.squares.map((square, index) => {
