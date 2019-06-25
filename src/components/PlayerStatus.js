06aaa4e188129840eb7a440c22176705d793f8d0
CalumCannon <calbo90@googlemail.com>
2019-06-25 15:41:18 +0100

    adds styling to the player status bar

--- src/components/PlayerStatus.js
+++ src/components/PlayerStatus.js
@@ -4,19 +4,36 @@ class PlayerStatus extends Component{
 
 constructor(props){
   super(props);
-
-
 }
 
-
+  generatePlayerStatusElements(player, activePlayer){
+    if(activePlayer){
+      return(
+        <div key={player.name}>
+        <div style={{backgroundColor : player.color}}><span>{player.name}  £{player.money}</span></div>
+        <div style={{backgroundColor : player.color}}><span>{player.colour}</span></div>
+        </div>
+      )
+    }else{
+      return(
+        <div key={player.name}>
+        <div><span>{player.name}  £{player.money}</span></div>
+        <div><span>{player.colour}</span></div>
+        </div>
+      )
+    }
+  }
 
   render() {
-    return(
-      <div>
 
-      <div><span>{this.props.players[0].name}  {this.props.players[0].money}</span></div>
-      <div><span>{this.props.players[0].colour}</span></div>
-      <div><span>{this.props.players[0].activePlayer}</span></div>
+    const players = this.props.players.map((player) => {
+        let activePlayerBool = player == this.props.activePlayer
+        return this.generatePlayerStatusElements(player,activePlayerBool)
+    })
+
+    return(
+      <div className="playerStatus">
+      {players}
       </div>
 
     );
