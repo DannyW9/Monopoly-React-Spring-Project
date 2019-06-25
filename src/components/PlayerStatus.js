f10075e65491e9524152fa00c3ae7429eab257e5
lindsey <lj0978@hotmail.co.uk>
2019-06-25 11:29:55 +0100

    player name and money shows in ststus

--- src/components/PlayerStatus.js
+++ src/components/PlayerStatus.js
@@ -10,13 +10,19 @@ constructor(props){
 
 
 
-render() {
-      return (
-        <p>Yay</p>
+  render() {
+    return(
+      <div>
 
-      );
-  }
+      <div><span>{this.props.players[0].name}  {this.props.players[0].money}</span></div>
+      <div><span>{this.props.players[0].colour}</span></div>
+      <div><span>{this.props.players[0].activePlayer}</span></div>
+      </div>
+
+    );
+
+
 }
-
+}
 
 export default PlayerStatus
