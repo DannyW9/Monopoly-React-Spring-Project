09c9f247b2ef663ae4fae8b30464cf2a5ac48770
Danny Welsh <dannywelsh400@hotmail.com>
2019-06-25 16:27:55 +0100

    Removes instance of Player Form from App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
 import Game from './containers/Game'
-import PlayerForm from './components/PlayerForm';
 
 class App extends Component {
   render(){
   return (<div>
     <Game />
-    <PlayerForm/>
     </div>)
 }
 }
